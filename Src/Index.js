8f9be572d269c8b2ee86000adb12a2580136bae8
Chiiorrma <chiumuo76@gmail.com>
2023-11-15 04:12:04 -0800

    added a new html file

---
+++ Src/Index.js
@@ -0,0 +1,83 @@
+const change = document.querySelector(".image1");
+
+change.addEventListener("click", (event) => {
+  event.target.style.background = "green";
+  event.target.style.color = "red";
+});
+
+const change2 = document.querySelector(".image2");
+
+change2.addEventListener("click", (event) => {
+  event.target.style.background = "yellow";
+  event.target.style.color = "black";
+});
+
+const change3 = document.querySelector(".image3");
+
+change3.addEventListener("click", (event) => {
+  event.target.style.background = "black";
+  event.target.style.color = "white";
+});
+
+const change4 = document.querySelector(".image4");
+
+change4.addEventListener("click", (event) => {
+  event.target.style.background = "purple";
+  event.target.style.color = "yellow";
+});
+
+const change5 = document.querySelector(".image5");
+
+change5.addEventListener("click", (event) => {
+  event.target.style.background = "cyan";
+  event.target.style.color = "grey";
+});
+
+const change6 = document.querySelector(".image6");
+
+change6.addEventListener("click", (event) => {
+  event.target.style.background = "red";
+  event.target.style.color = "pink";
+});
+
+const change7 = document.querySelector(".image7");
+
+change7.addEventListener("click", (event) => {
+  event.target.style.background = "green";
+  event.target.style.color = "red";
+});
+
+const change8 = document.querySelector(".image8");
+
+change8.addEventListener("click", (event) => {
+  event.target.style.background = "yellow";
+  event.target.style.color = "black";
+});
+
+const change9 = document.querySelector(".image9");
+
+change9.addEventListener("click", (event) => {
+  event.target.style.background = "black";
+  event.target.style.color = "white";
+});
+
+const change10 = document.querySelector(".image10");
+
+change10.addEventListener("click", (event) => {
+  event.target.style.background = "purple";
+  event.target.style.color = "yellow";
+});
+
+const change11 = document.querySelector(".image11");
+
+change11.addEventListener("click", (event) => {
+  event.target.style.background = "cyan";
+  event.target.style.color = "grey";
+});
+
+const change12 = document.querySelector(".image12");
+
+change12.addEventListener("click", (event) => {
+  event.target.style.background = "red";
+  event.target.style.color = "pink";
+});
